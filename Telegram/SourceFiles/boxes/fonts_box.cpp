Kylmakalle/kotatogram-desktop

Original font height option

Telegram/SourceFiles/boxes/fonts_box.cpp
@@ -23,6 +23,7 @@ FontsBox::FontsBox(QWidget* parent)
 , _semiboldFontName(this, st::defaultInputField, tr::ktg_fonts_semibold())
 , _semiboldIsBold(this, tr::ktg_fonts_semibold_is_bold(tr::now), cSemiboldFontIsBold())
 , _monospacedFontName(this, st::defaultInputField, tr::ktg_fonts_monospaced())
+, _useOriginalMetrics(this, tr::ktg_fonts_use_original_metrics(tr::now), cUseOriginalMetrics())
 , _about(st::boxWidth - st::boxPadding.left() * 1.5)
 {
 }
@@ -55,8 +56,9 @@ void FontsBox::prepare() {
 		+ _semiboldFontName->height()
 		+ _semiboldIsBold->height()
 		+ _monospacedFontName->height()
+		+ _useOriginalMetrics->height()
 		+ _aboutHeight
-		+ st::boxLittleSkip * 2);
+		+ st::boxLittleSkip * 3);
 }
 
 
@@ -70,7 +72,8 @@ void FontsBox::paintEvent(QPaintEvent *e) {
 		+ _semiboldFontName->height()
 		+ _semiboldIsBold->height()
 		+ _monospacedFontName->height()
-		+ st::boxLittleSkip * 2;
+		+ _useOriginalMetrics->height()
+		+ st::boxLittleSkip * 3;
 	p.setPen(st::windowSubTextFg);
 	_about.drawLeft(p, st::boxPadding.left(), abouty, w, width());
 
@@ -90,6 +93,8 @@ void FontsBox::resizeEvent(QResizeEvent *e) {
 	_semiboldIsBold->moveToLeft(st::boxPadding.left(), _semiboldFontName->y() + _semiboldFontName->height() + st::boxLittleSkip);
 	_monospacedFontName->resize(w, _monospacedFontName->height());
 	_monospacedFontName->moveToLeft(st::boxPadding.left(), _semiboldIsBold->y() + _semiboldIsBold->height());
+	_useOriginalMetrics->resize(w, _useOriginalMetrics->height());
+	_useOriginalMetrics->moveToLeft(st::boxPadding.left(), _monospacedFontName->y() + _monospacedFontName->height() + st::boxLittleSkip);
 }
 
 void FontsBox::setInnerFocus() {
@@ -102,6 +107,7 @@ void FontsBox::save() {
 	const auto semiboldFont = _semiboldFontName->getLastText().trimmed();
 	const auto semiboldIsBold = _semiboldIsBold->checked();
 	const auto monospacedFont = _monospacedFontName->getLastText().trimmed();
+	const auto useOriginalMetrics = _useOriginalMetrics->checked();
 
 	const auto changeFonts = [=] {
 		cSetUseSystemFont(useSystemFont);
@@ -109,6 +115,7 @@ void FontsBox::save() {
 		cSetSemiboldFont(semiboldFont);
 		cSetSemiboldFontIsBold(semiboldIsBold);
 		cSetMonospaceFont(monospacedFont);
+		cSetUseOriginalMetrics(useOriginalMetrics);
 		KotatoSettings::Write();
 		App::restart();
 	};

kotatogram/kotatogram-archived

Telegram/SourceFiles/boxes/fonts_box.cpp
@@ -19,6 +19,7 @@ https://github.com/kotatogram/kotatogram-desktop/blob/dev/LEGAL
 
 FontsBox::FontsBox(QWidget* parent)
 : _useSystemFont(this, tr::ktg_fonts_use_system_font(tr::now), cUseSystemFont())
+, _useOriginalMetrics(this, tr::ktg_fonts_use_original_metrics(tr::now), cUseOriginalMetrics())
 , _mainFontName(this, st::defaultInputField, tr::ktg_fonts_main())
 , _semiboldFontName(this, st::defaultInputField, tr::ktg_fonts_semibold())
 , _semiboldIsBold(this, tr::ktg_fonts_semibold_is_bold(tr::now), cSemiboldFontIsBold())
@@ -51,12 +52,13 @@ void FontsBox::prepare() {
 	_aboutHeight = _about.countHeight(st::boxWidth - st::boxPadding.left() * 1.5);
 
 	setDimensions(st::boxWidth, _useSystemFont->height()
+		+ _useOriginalMetrics->height()
 		+ _mainFontName->height()
 		+ _semiboldFontName->height()
 		+ _semiboldIsBold->height()
 		+ _monospacedFontName->height()
 		+ _aboutHeight
-		+ st::boxLittleSkip * 2);
+		+ st::boxLittleSkip * 3);
 }
 
 
@@ -66,11 +68,12 @@ void FontsBox::paintEvent(QPaintEvent *e) {
 	Painter p(this);
 	int32 w = st::boxWidth - st::boxPadding.left() * 1.5;
 	int32 abouty = _useSystemFont->height()
+		+ _useOriginalMetrics->height()
 		+ _mainFontName->height()
 		+ _semiboldFontName->height()
 		+ _semiboldIsBold->height()
 		+ _monospacedFontName->height()
-		+ st::boxLittleSkip * 2;
+		+ st::boxLittleSkip * 3;
 	p.setPen(st::windowSubTextFg);
 	_about.drawLeft(p, st::boxPadding.left(), abouty, w, width());
 
@@ -82,8 +85,10 @@ void FontsBox::resizeEvent(QResizeEvent *e) {
 	int32 w = st::boxWidth - st::boxPadding.left() - st::boxPadding.right();
 	_useSystemFont->resize(w, _useSystemFont->height());
 	_useSystemFont->moveToLeft(st::boxPadding.left(), 0);
+	_useOriginalMetrics->resize(w, _useOriginalMetrics->height());
+	_useOriginalMetrics->moveToLeft(st::boxPadding.left(), _useSystemFont->y() + _useSystemFont->height() + st::boxLittleSkip);
 	_mainFontName->resize(w, _mainFontName->height());
-	_mainFontName->moveToLeft(st::boxPadding.left(), _useSystemFont->y() + _useSystemFont->height() + st::boxLittleSkip);
+	_mainFontName->moveToLeft(st::boxPadding.left(), _useOriginalMetrics->y() + _useOriginalMetrics->height() + st::boxLittleSkip);
 	_semiboldFontName->resize(w, _semiboldFontName->height());
 	_semiboldFontName->moveToLeft(st::boxPadding.left(), _mainFontName->y() + _mainFontName->height());
 	_semiboldIsBold->resize(w, _semiboldIsBold->height());
@@ -98,6 +103,7 @@ void FontsBox::setInnerFocus() {
 
 void FontsBox::save() {
 	const auto useSystemFont = _useSystemFont->checked();
+	const auto useOriginalMetrics = _useOriginalMetrics->checked();
 	const auto mainFont = _mainFontName->getLastText().trimmed();
 	const auto semiboldFont = _semiboldFontName->getLastText().trimmed();
 	const auto semiboldIsBold = _semiboldIsBold->checked();
@@ -105,6 +111,7 @@ void FontsBox::save() {
 
 	const auto changeFonts = [=] {
 		cSetUseSystemFont(useSystemFont);
+		cSetUseOriginalMetrics(useOriginalMetrics);
 		cSetMainFont(mainFont);
 		cSetSemiboldFont(semiboldFont);
 		cSetSemiboldFontIsBold(semiboldIsBold);